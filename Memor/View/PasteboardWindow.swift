08283cb3d081816388e0aa6637b9ec5c94788bbb
tcarrill <thomas.carrill@gmail.com>
2018-03-12 17:43:24 -0400

    Removed xcuserdata

---
+++ Memor/View/PasteboardWindow.swift
@@ -0,0 +1,19 @@
+//
+//  PasteboardWindow.swift
+//  Memor
+//
+//  Created by Thomas Carrill on 3/11/18.
+//  Copyright © 2018 devcellar. All rights reserved.
+//
+
+import Cocoa
+
+class PasteboardWindow: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
---
+++ Memor/ViewModel/PasteboardWindowViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  PasteboardWindowViewModel.swift
+//  Memor
+//
+//  Created by Thomas Carrill on 3/11/18.
+//  Copyright © 2018 devcellar. All rights reserved.
+//
+
+import Foundation
